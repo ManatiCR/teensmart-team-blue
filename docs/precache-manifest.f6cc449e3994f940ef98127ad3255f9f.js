290d02aa03ac6cc4dec072781e983223a5ada647
mledezma <marcoledezmacordero09@gmail.com>
2019-08-07 15:07:47 -0600

    Adding package script for gh pages and adding github pages target folder

---
+++ docs/precache-manifest.f6cc449e3994f940ef98127ad3255f9f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d74af783f8d96e99e7c262af41cdcca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c0fd64ce49dc3f27218",
+    "url": "/static/css/main.1da1285f.chunk.css"
+  },
+  {
+    "revision": "34e1f1716e7316948318",
+    "url": "/static/js/2.494ccf5e.chunk.js"
+  },
+  {
+    "revision": "3c0fd64ce49dc3f27218",
+    "url": "/static/js/main.1e457b57.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfad5cf504556de06aedfcc768eb5672",
+    "url": "/static/media/logoweb.cfad5cf5.png"
+  },
+  {
+    "revision": "7b19e7157fefd3ea19c0934075edb988",
+    "url": "/static/media/logoweb2.7b19e715.png"
+  },
+  {
+    "revision": "2320b5f0c540b88f5fb512db51a68f1b",
+    "url": "/static/media/map.2320b5f0.jpg"
+  },
+  {
+    "revision": "0df5666876be81cc89711116af48b2e1",
+    "url": "/static/media/teenagers.0df56668.jpg"
+  }
+]);
